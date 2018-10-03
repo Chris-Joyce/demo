24cb79b6017b65ce05b2b812b267bc61351edcee
Chris-Joyce <36461109+Chris-Joyce@users.noreply.github.com>
2018-10-03 15:28:47 -0400

    Add files via upload

---
+++ demo/demo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using demo.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private List<Cert> cert;
+        public HomeController()
+        {
+            cert = new List<Cert>
+            {
+                new Cert()
+                { Id = 000, FirstName = "testFirst0", LastName = "testLast0", CertName = "ExpiredCert", ExpDate = "2017-01-01", Email = "test0@emal.com" },
+                new Cert()
+                { Id = 002, FirstName = "testFirst2", LastName = "testLast2", CertName = "15DayCert", ExpDate = "2018-10-15", Email = "test2@emal.com" },
+                new Cert()
+                { Id = 001, FirstName = "testFirst1", LastName = "testLast1", CertName = "30DayCert", ExpDate = "2018-10-25", Email = "test1@emal.com" },
+                new Cert()
+                { Id = 003, FirstName = "testFirst3", LastName = "testLast3", CertName = "Over30DayCert", ExpDate = "2020-01-01", Email = "test3@emal.com" }
+            };
+        }
+
+        public ActionResult Index()
+        {
+            return View(cert);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ demo/demo/Models/Cert.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace demo.Models
+{
+    public class Cert
+    {
+        public int Id { get; set; }//not used
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string CertName { get; set; }
+        public string ExpDate { get; set; }
+        public string Email { get; set; }//not used yet
+      
+    }
+}
